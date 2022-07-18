03f04de18b812b18099847f2011a07282e7761b7
reoseah <reoseah9@gmail.com>
2022-07-18 19:31:24 +0200

    Test some EnderIO inspired machine textures

--- src/main/java/io/github/reoseah/spacefactory/SF.java
+++ src/main/java/io/github/reoseah/spacefactory/SF.java
@@ -1,6 +1,9 @@
 package io.github.reoseah.spacefactory;
 
 import io.github.reoseah.spacefactory.feature.generator.fuel.GeneratorBlock;
+import io.github.reoseah.spacefactory.feature.generator.solar.SolarPanelBlock;
+import io.github.reoseah.spacefactory.feature.machine.electric_furnace.ElectricFurnaceBlock;
+import io.github.reoseah.spacefactory.feature.machine.grinder.GrinderBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -13,6 +16,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
@@ -37,16 +41,24 @@ public class SF implements ModInitializer {
     }
 
     public static class Blocks {
-        public static final Block REFINED_IRON_BLOCK = new Block(AbstractBlock.Settings.of(Material.METAL).strength(3F, 6F).sounds(BlockSoundGroup.METAL).requiresTool());
+        public static final Block GENERATOR = new GeneratorBlock(AbstractBlock.Settings.of(Material.METAL).strength(3F).sounds(BlockSoundGroup.METAL).luminance(state -> state.get(Properties.LIT) ? 14 : 0));
+        public static final Block SOLAR_PANEL = new SolarPanelBlock(AbstractBlock.Settings.of(Material.METAL, MapColor.BLUE).strength(3F).sounds(BlockSoundGroup.METAL));
 
-        public static final Block GENERATOR = new GeneratorBlock(AbstractBlock.Settings.of(Material.METAL).strength(3F).sounds(BlockSoundGroup.METAL).requiresTool());
-        public static final Block SOLAR_PANEL = new GeneratorBlock(AbstractBlock.Settings.of(Material.METAL, MapColor.BLUE).strength(3F).sounds(BlockSoundGroup.METAL).requiresTool());
+        public static final Block ELECTRIC_FURNACE = new ElectricFurnaceBlock(AbstractBlock.Settings.of(Material.METAL).strength(3F).sounds(BlockSoundGroup.METAL).luminance(state -> state.get(Properties.LIT) ? 13 : 0));
+        public static final Block GRINDER = new GrinderBlock(AbstractBlock.Settings.of(Material.METAL).strength(3F).sounds(BlockSoundGroup.METAL));
+        public static final Block EXTRACTOR = new GrinderBlock(AbstractBlock.Settings.of(Material.METAL).strength(3F).sounds(BlockSoundGroup.METAL));
+
+        public static final Block REFINED_IRON_BLOCK = new Block(AbstractBlock.Settings.of(Material.METAL).strength(3F, 6F).sounds(BlockSoundGroup.METAL));
 
         public static void register() {
             register("refined_iron_block", REFINED_IRON_BLOCK);
 
             register("generator", GENERATOR);
             register("solar_panel", SOLAR_PANEL);
+
+            register("electric_furnace", ELECTRIC_FURNACE);
+            register("grinder", GRINDER);
+            register("extractor", EXTRACTOR);
         }
 
         private static void register(String name, Block entry) {
@@ -55,17 +67,24 @@ public class SF implements ModInitializer {
     }
 
     public static class Items {
-        public static final Item REFINED_IRON_BLOCK = new BlockItem(Blocks.REFINED_IRON_BLOCK, settings(DECORATION));
-
         public static final Item GENERATOR = new BlockItem(Blocks.GENERATOR, settings(TECHNOLOGY));
         public static final Item SOLAR_PANEL = new BlockItem(Blocks.SOLAR_PANEL, settings(TECHNOLOGY));
+        public static final Item ELECTRIC_FURNACE = new BlockItem(Blocks.ELECTRIC_FURNACE, settings(TECHNOLOGY));
+        public static final Item GRINDER = new BlockItem(Blocks.GRINDER, settings(TECHNOLOGY));
+        public static final Item EXTRACTOR = new BlockItem(Blocks.EXTRACTOR, settings(TECHNOLOGY));
 
         public static final Item REFINED_IRON_INGOT = new Item(settings(TECHNOLOGY));
+
+        public static final Item REFINED_IRON_BLOCK = new BlockItem(Blocks.REFINED_IRON_BLOCK, settings(DECORATION));
 
         public static void register() {
             register("refined_iron_block", REFINED_IRON_BLOCK);
             register("generator", GENERATOR);
             register("solar_panel", SOLAR_PANEL);
+
+            register("electric_furnace", ELECTRIC_FURNACE);
+            register("grinder", GRINDER);
+            register("extractor", EXTRACTOR);
 
             register("refined_iron_ingot", REFINED_IRON_INGOT);
         }
---
+++ src/main/java/io/github/reoseah/spacefactory/feature/machine/grinder/GrinderBlock.java
@@ -0,0 +1,44 @@
+package io.github.reoseah.spacefactory.feature.machine.grinder;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.Direction;
+
+public class GrinderBlock extends Block {
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+    public static final BooleanProperty LIT = Properties.LIT;
+
+    public GrinderBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.getDefaultState().with(FACING, Direction.NORTH).with(LIT, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING, LIT);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+}
